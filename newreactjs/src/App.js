635e8ed05052f515c26cc56bbd1e1f141044a755
xianlinsanity <xian.omana@gmail.com>
2023-11-08 21:53:25 +0800

    ok

--- newreactjs/src/App.js
+++ newreactjs/src/App.js
@@ -1,21 +1,59 @@
 import React, {useState} from 'react';
 import './App.css';
+import logo from './logo.png';
 
 import ListComponent from './components/list';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const buttonClick = () => {
-    setCounter(counter + 1);
-  }
-  const anotherElement = <h3>hi from h3</h3>
+  const onClick = function(){
+    const username = document.getElementById("username")
+    const password = document.getElementById("password")
+    const email = document.getElementById("email")
+    fetch('http://localhost:3000/register', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username.value,
+            password: password.value,
+            email: email.value,
+        })
+    }).then(function(result){
+          return result.json()
+      }).then(function(result){
+          console.log('result: ', result)
+          if(result.success){
+                document.getElementById("message").innerHTML = "Registered Successfully!";
+            } else {
+                document.getElementById("message").innerHTML = "Sorry! Username already in used.";
+            }
+             // Prevent the form from submitting
+             return false;
+          })
+}
+  const anotherElement = <h5 className='p1'>Registration Form</h5>
+  const newlogo = <img src={logo} class="img-thumbnail logo" ></img>
   return (
-    <div className="App container">
-      <p>from component text</p>
-      {anotherElement}
-      <button onClick={buttonClick} className='btn btn-info'>click me</button>
-      <h1>button clicked #{counter}</h1>
-      <ListComponent></ListComponent>
+    <div className='container'>
+      {newlogo}
+        <h2 className='p1'>Registration Form</h2>
+        <br/>
+        <br/>
+        <p className='p1'>Please fill out the information to register!</p>
+        <br/>
+        <div>
+            <input type="text" id="username" placeholder="Username"/>
+            <input type="password" id="password" placeholder="Password"/>
+            <input type="text" id="email" placeholder="Email Address"/>
+            <button id="register" onClick={onClick}>Register</button>
+            <br/>
+            <br/>
+            <p id="message"></p>
+        </div>
+        <br/>
+        <p className='p1'>Please click on <a href="login.html">Login</a> if you are already registered.</p>
     </div>
   )
 }
