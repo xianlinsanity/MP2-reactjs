fac2159654c115279a5b4452e61f0c1b52af0a0b
xianlinsanity <xian.omana@gmail.com>
2023-11-09 21:12:35 +0800

    ok

--- newreactjs/src/App.js
+++ newreactjs/src/App.js
@@ -1,105 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import logo from './logo.png';
-
-import ListComponent from './components/list';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Registration from './registration/registration';
+import Login from './registration/login/login';
 
 function App() {
-  const onClick = function(){
-    const username = document.getElementById("username")
-    const password = document.getElementById("password")
-    const email = document.getElementById("email")
-    fetch('http://localhost:3000/register', {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: username.value,
-            password: password.value,
-            email: email.value,
-        })
-    }).then(function(result){
-          return result.json()
-      }).then(function(result){
-          console.log('result: ', result)
-          if(result.success){
-                document.getElementById("message").innerHTML = "Registered Successfully!";
-            } else {
-                document.getElementById("message").innerHTML = "Sorry! Username already in used.";
-            }
-             // Prevent the form from submitting
-             return false;
-          })
-  const onClick1 = function(){
-    const username = document.getElementById('username')
-        const password = document.getElementById('password')
-        fetch('http://localhost:3000/login', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: username.value,
-                password: password.value,
-            })
-        }).then(function(result){
-              return result.json()
-          }).then(function(result){
-              console.log('result: ', result)
-              if(result.success){
-                document.getElementById("message").innerHTML = "Login Successful! Welcome!";
-            } else {
-                document.getElementById("message").innerHTML = "Login Unsuccessful. Please check your credentials.";
-            }
-
-            // Prevent the form from submitting
-            return false;
-          })
-}
-  }
-  const anotherElement = <h5 className='p1'>Registration Form</h5>
-  const newlogo = <img src={logo} class="img-thumbnail logo" ></img>
   return (
-    <div className='container'>
-      {newlogo}
-        <h2 className='p1'>Registration Form</h2>
-        <br/>
-        <br/>
-        <p className='p1'>Please fill out the information to register!</p>
-        <br/>
-        <div>
-            <input type="text" id="username" placeholder="Username"/>
-            <input type="password" id="password" placeholder="Password"/>
-            <input type="text" id="email" placeholder="Email Address"/>
-            <button id="register" onClick={onClick1}>Register</button>
-            <br/>
-            <br/>
-            <p id="message"></p>
-        </div>
-        <br/>
-        <p className='p1'>Please click on <a href="login.html">Login</a> if you are already registered.</p>
-        <br/>
-        <br/>
-        <br/>
-        <h2 className='p1'>Member's Login</h2>
-        <p className='p1'>Please fill out the information needed.</p>
-        <br/>
-        <form>
-            <input type="text" id="username" placeholder="Username"/>
-            <br/>
-            <input type="password" id="password" placeholder="Password"/>
-            <br/>
-            <button id="login" onClick={onClick1}>Login</button>
-            <br/>
-            <br/>
-            <p id="message"></p>
-        </form>
-        <br/>
-        <p className='p1'>Not yet a member? Please click <a href="registration.html"> here</a> to register and avail discounts and freebies!</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login/>}/>
+        <Route path="registration" element={<Registration/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ newreactjs/src/registration/login/login.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import logo from '../../logo.png'
+import { Link } from 'react-router-dom';
+
+function Login() {
+    const loginbtnclick = function(){
+        const username = document.getElementById('username')
+            const password = document.getElementById('password')
+            fetch('http://localhost:3000/login', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username.value,
+                    password: password.value,
+                })
+            }).then(function(result){
+                  return result.json()
+              }).then(function(result){
+                  console.log('result: ', result)
+                  if(result.success){
+                    document.getElementById("message").innerHTML = "Login Successful! Welcome!";
+                } else {
+                    document.getElementById("message").innerHTML = "Login Unsuccessful. Please check your credentials.";
+                }
+    
+                // Prevent the form from submitting
+                return false;
+              })
+    }
+    const newlogo = <img src={logo} class="img-thumbnail logo" ></img>
+return (
+    <div className='container'>
+        {newlogo}
+        <br/>
+        <br/>
+        <h2 className='p1'>Member's Login</h2>
+        <p className='p1'>Please fill out the information needed.</p>
+        <br/>
+        <div>
+            <input type="text" id="username" placeholder="Username"/>
+            <br/>
+            <input type="password" id="password" placeholder="Password"/>
+            <br/>
+            <button id="login" onClick={loginbtnclick} className='btn btn-secondary btn-lg'>Login</button>
+            <br/>
+            <p id="message" className='p1'></p>
+        </div>
+        <br/>
+        <p className='p1'>Not yet a member? Please click<Link to="registration">here</Link>to register and avail discounts and freebies!</p>
+    </div>
+  )
+}
+
+export default Login;
